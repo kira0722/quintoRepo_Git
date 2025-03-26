e03ca0c63178e884250d75cb6d75c726cd7a4891
Samuel Quirama Pino <samuel.quirama@bebolder.local>
2025-03-26 10:37:19 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintoRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Samuel Quirama",                         # Tu nombre
+    author_email="squirama2003@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/kira0722/quintoRepo_Git",     # URL del proyecto
+)
